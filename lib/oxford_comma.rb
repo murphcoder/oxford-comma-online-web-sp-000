fbe13e1d2dbf0f6db7da260238ce137dcc5f858d
Sean Murphy <filmmakersean@fastmail.net>
2019-05-03 17:29:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return "#{array[0]}"
+    elsif array.count == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    return "#{array[0..-2].join(", ")}, and #{array[-1]}"
+  end
 end
